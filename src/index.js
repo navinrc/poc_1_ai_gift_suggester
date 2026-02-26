5c30f5ffa4917e029d014c7eb59e57333cfc4fec
navinrc <navinrc98@gmail.com>
2026-02-26 11:17:41 +0530

    enh: added system prompts and context for the model

--- src/index.js
+++ src/index.js
@@ -16,21 +16,46 @@ const invoke = async () => {
   outputEl.textContent = "Thinking...";
 
   try {
-    const response = await openai.chat.completions.create({
+    const messages = [
+      {
+        role: "user",
+        content: `Suggest some gifts for someone who loves hiphop music.`,
+      },
+    ];
+    // system prompts are instructions for the model to generate responses that actually matter instead of user instructing these every time.
+    messages.push({
+      role: "system",
+      content: `Make these suggestions thoughtful and practical. Your response must be under 100 words. 
+Skip intros and conclusions. Only output gift suggestions.`
+    })
+
+    const firstResponse = await openai.chat.completions.create({
       model: process.env.AI_MODEL,
-      messages: [
-        {
-          role: "user",
-          content: `Suggest some gifts for someone who loves hiphop music. 
-Make these suggestions thoughtful and practical. Your response must be under 100 words. 
-Skip intros and conclusions. Only output gift suggestions.`, // crisp prompt
-        },
-      ],
+      messages,
       max_completion_tokens: 256, // explicitly setting 256 tokens
       reasoning_effort: "minimal", // this limits the reasoning token to 0 ( minimal )
     });
-    console.log("response", JSON.stringify(response));
-    outputEl.textContent = response.choices[0].message.content;
+    
+    console.log("response", JSON.stringify(firstResponse));
+    outputEl.textContent = firstResponse.choices[0].message.content;
+
+    const firstAssistantMessage = firstResponse.choices[0].message;
+    messages.push(firstAssistantMessage);
+
+    messages.push({
+      role: "user",
+      content: "More budget friendly. Less than $40.",
+    });
+    // Send second chat completions request with extended messages array
+    const secondResponse = await openai.chat.completions.create({
+      model: process.env.AI_MODEL,
+      messages,
+    });
+
+    console.log("Budget friendly suggestions:");
+    console.log(secondResponse.choices[0].message.content);
+
+    outputEl.textContent = `${outputEl.textContent}  \n Budget Option under 40$ \n ${secondResponse.choices[0].message.content}`;
   } catch (error) {
     if (error.status === 401 || error.status === 403) {
       outputEl.textContent = "Authentication error: Check your AI_KEY.";
